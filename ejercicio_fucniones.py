74221c460ffc1d70a3030455386b34b2fdfd3e78
Cristóbal Varas Polanco <105750311+Crist0b4l@users.noreply.github.com>
2024-09-26 23:14:55 -0300

    segundo commit
    Agregando mas ejercicios al codigo

--- ejercicio_fucniones.py
+++ ejercicio_fucniones.py
@@ -1,6 +1,10 @@
 '''Ejercicios funciones'''
 
 
+import math
+import re
+
+
 def mostrar_menu():
     print("1. Calcuiar iva")
     print("2. Aplicar descuento")
@@ -53,6 +57,86 @@ while True:
         calcular_imc(peso, estatura)
     elif opcion == 4:
         print("Saliendo...")
+        break
+    else:
+        print("Seleccione una opción válida")
+
+
+# Ejercicio 2 menu con operaciones matematicas
+
+
+def menu2():
+    print("Menú")
+    print("1. Número primo")
+    print("2. Factorial")
+    print("3. Palíndrome")
+    print("4. Salir")
+
+
+def num_primo(num):
+    if num <= 1:
+        print(f"{num} No es un número primo")
+        return False
+    if num == 2:
+        print(f"{num} Es el único número par primo")
+        return True
+    if num % 2 == 0:
+        print(f"{num} No es un número primo")
+        return False
+    for n in range(3, num):
+        if num % n == 0:
+            print(f"{num} No es un número primo, es divisible por {n}")
+            return False
+    print(f"{num} Es un número primo")
+    return True
+
+
+def factorial(num):
+    secuencia = "*".join(str(i) for i in range(num, 0, -1))
+    resultado = math.factorial(num)
+    print(f"El factorial de {num} es: {secuencia} = {resultado}")
+
+
+def es_palindromo(frase):
+    frase_incluida = re.sub(r'[^a-zA-Z0-9]', '', frase.lower())
+    if frase_incluida == frase_incluida[::-1]:
+        print(f"{frase} Es un palíndromo")
+    else:
+        print(f"{frase} No es un palíndromo")
+
+
+while True:
+    menu2()
+    print("Elige una opción")
+    opcion = int(input())
+
+    if opcion == 1:
+        while True:
+            num = int(
+                input("Ingrese un número entero entre 1 y 15 para verificar si es primo: "))
+            if num < 1 or num > 15:
+                print("Por favor ingrese un número dentro del rango (1-15)")
+            else:
+                num_primo(num)
+                break
+
+    elif opcion == 2:
+        while True:
+            num = int(
+                input("Ingrese un número entero entre 3 y 10 para conocer su factorial: "))
+            if num < 3 or num > 10:
+                print("Por favor ingrese un número dentro del rango (3-10)")
+            else:
+                factorial(num)
+                break
+
+    elif opcion == 3:
+        frase = input("Ingrese una frase para comprobar si es un palíndromo: ")
+        es_palindromo(frase)
+
+    elif opcion == 4:
+        print("Saliendo del programa...")
+        print("Autor: Cristóbal Varas Polanco")
         break
     else:
         print("Seleccione una opción válida")
